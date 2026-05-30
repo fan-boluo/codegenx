ccaa4a24fe50e07fe255dacd197c179219f21ccf
fyx <fyxboluo@163.com>
2026-05-30 23:14:22 +0800

    顶部菜单栏显示

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { computed } from 'vue'
 import BasicLayout from '@/layouts/BasicLayout.vue'
-
-const route = useRoute()
-const isFullScreen = computed(() => {
-  const path = route.path
-  return path.startsWith('/app/chat')
-})
 </script>
 
 <template>
-  <router-view v-if="isFullScreen" />
-  <BasicLayout v-else />
+  <BasicLayout />
 </template>
